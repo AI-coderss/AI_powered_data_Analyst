45c98405b72d3b18905900f28f9452348d86a017
Mohammed Bahageel <Mohmmed.Bahageel@DSAH.SA>
2025-05-11 12:27:17 +0300

    my second commit

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/*": {"origins": "https://ai-powered-data-analyst-dsah.onrender.com"}})
 
 
 UPLOAD_FOLDER = 'uploads'
