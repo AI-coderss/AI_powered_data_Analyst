0f80e0381cf20ac656f6c74d4b49369e6ebb56f1
Mohammed Bahageel <Mohmmed.Bahageel@DSAH.SA>
2025-05-11 15:00:52 +0300

    my test commit

--- backend/app.py
+++ backend/app.py
@@ -70,4 +70,4 @@ Give a markdown summary of insights based on the data and the question.
 
 
 if __name__ == "__main__":
-     app.run(host="127.0.0.1", port=5050, debug=True)
+     app.run(host="127.0.0.1",  debug=True)
