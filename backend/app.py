6888e4ff0b0b80f8553fa79d3e6f6312b70f3ebb
ahsanjamali <ahsanjamali7042@gmail.com>
2025-06-04 16:32:00 +0500

    backend production url

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,7 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://ai-powered-data-analyst-2l0a.onrender.com"}})
+CORS(app, resources={r"/*": {"origins": "https://ai-powered-data-analyst-frontend.onrender.com"}})
 
 
 UPLOAD_FOLDER = 'uploads'
